dd65cab2a6bafaa531729fd1bae322bf1ccd1ba0
mamun6 <74857061+mamun6@users.noreply.github.com>
2025-05-18 08:27:44 +0000

    updated axios

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,6 +3,10 @@ import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {dummyProducts} from "../assets/assets";
 import toast from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
@@ -88,7 +92,7 @@ export const AppContextProvider = ({ children }) => {
     const value = { navigate, user, setUser, isSeller, 
         setIsSeller, showUserLogin, setShowUserLogin,products,currency,
         addToCart , updateCartItem ,removeFromCart,cartItems ,searchQuery,
-        setSearchQuery , getCartAmount, getCartCount
+        setSearchQuery , getCartAmount, getCartCount,axios
     }
 
     return <AppContext.Provider value={value}>
